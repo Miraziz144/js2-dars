940874fda207af615372c7330d0360e4160aec7f
Miraziz144 <nurullayevmiraziz4@gmail.com>
2024-06-01 12:50:26 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+let strim = prompt ("Stirim typedagi malumot kiriting" )
+
+
+let num = prompt ("number typedagi malumot kiriting " )
+
+
+let boolen = prompt ("boolean typedagi malumot kirining " )
+
+
+alert ('hamma malumot togriligiga ishonchingiz komilmi ')
+
+
+console.log(strim);
+console.log(num);
+console.log(strim);
+console.log(ism);
